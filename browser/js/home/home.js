ad885d760ccf6c41f181bc8975f677664cda7099
dwschrashun <dwschrashun@gmail.com>
2015-09-24 11:37:14 -0400

    basic keyup function

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,11 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: function ($scope) {
+        	$scope.registerKey = function (keyEvent) {
+        		console.log("here", keyEvent);
+        	};
+        }
     });
 });
