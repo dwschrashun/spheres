2d98265c9d21c1e4841da4449cdec353207fb784
dwschrashun <dwschrashun@gmail.com>
2015-09-24 20:37:41 -0400

    key presses actually play notes

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,70 +2,74 @@ app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
         templateUrl: 'js/home/home.html',
-        controller: function ($scope, $window) {
-        	$scope.registerKey = function (keyEvent) {
-        		console.log("KeyCode: ", keyEvent.keyCode);
+        controller: function ($scope, $window, SoundFactory) {
+        	var i = 0;
+        	$scope.playKey = function (keyEvent) {
+        		console.log("Keynote", SoundFactory.getKeyNote(keyEvent.keyCode));
+        		playNote(SoundFactory.getKeyNote(keyEvent.keyCode));
         	};
-        	$scope.play = function (clickEvent) {
+        	$scope.playAuto = function (clickEvent) {
         		console.log("Click X, Y: " + clickEvent.x + ", " + clickEvent.y);
-    //     		$scope.oscillator.start();
-				// $scope.oscillator.stop($scope.context.currentTime + 1);
-        	};
+        		playNotes();
+        		i = 0;
+			};
+
+			function playNotes () {
+				setTimeout(function () {
+					console.log("playing:", i);
+	    			$scope.nextNotes[i].connect($scope.context.destination);
+	    			$scope.nextNotes[i].start();
+	    			$scope.nextNotes[i].stop($scope.context.currentTime + $scope.nextNotes[i].duration);
+    				i++;
+    				if (i <$scope.nextNotes.length) {
+    					playNotes();
+    				}
+    			}, $scope.nextNotes[i].duration * 1000);
+    		}
+
+    		function playNote (noteObj) {
+    			var note = createNote(noteObj);
+    			note.connect($scope.context.destination);
+    			note.start();
+    			note.stop($scope.context.currentTime + note.duration);
+    		}
+
+
+   				// var time = 0;
+				// for (var i = 0; i < $scope.nextNotes.length; i++) {
+				//    time += 1000;
+				//    setTimeout(function(i) {
+				//        return function() {
+				//            console.log($scope.nextNotes[i]);
+				//        }
+				//    }(i), time);
+				// }
+        	
         	$scope.setAudio = function () {
         		$window.AudioContext = $window.AudioContext||$window.webkitAudioContext;
     			$scope.context = new AudioContext();
     			$scope.oscillator = $scope.context.createOscillator();
 				$scope.oscillator.type = 'square';
-				$scope.oscillator.frequency.value = 3000; // value in hert
-				$scope.oscillator.connect($scope.context.destination);
         	};
 
-        	function createNote (note, length) {
-        		var note = $scope.context.createOscillator();
-        		note.type = "sine";
-        		note.frequency.value = getFrequency(note);
-        		note.frequency.value = 440;
-        		note.length = length;
-        		return note;
-        	}
+		    function createNote (noteObj) {
+		    	console.log("CreateNote:", noteObj);
+				var note = $scope.context.createOscillator();
+				note.type = "sine";
+				note.frequency.value = noteObj.freq;
+				note.duration = noteObj.duration;
+				return note;
+			}
 
-        	//turn each note in an array of notes into notes objects that the oscillator can play
-        	function addNotes (arr) {
-        		noteBank.push(
-        			arr.map(function (current) {
-        				return createNote(current.note, current.length);
-        			})
-        		);
-        	}
+			//turn each note in an array of notes into notes objects that the oscillator can play
+			function addNotes (arr) {
+				return arr.map(function (current) {
+						return createNote(current);
+				});
+			}
 
-        	var noteBank = [];
-
-        	var notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
-
-			var getFrequency = function (note) {
-			  
-			       var octave,
-			       keyNumber;
-
-			   if (note.length === 3) {
-			       octave = note.charAt(2);
-			   } else {
-			       octave = note.charAt(1);
-			   }
-
-			   keyNumber = notes.indexOf(note.slice(0, -1));
-
-			   if (keyNumber < 3) {
-			       keyNumber = keyNumber + 12 + ((octave - 1) * 12) + 1; 
-			   } else {
-			       keyNumber = keyNumber + ((octave - 1) * 12) + 1; 
-			   }
-
-			   // Return frequency of note
-			   return 440 * Math.pow(2, (keyNumber- 49) / 12);
-			};
- 
         	$scope.setAudio();
+			$scope.nextNotes = addNotes(SoundFactory.getNotes());
         },
         resolve : {
         }
