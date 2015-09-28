48689ddf5f32e7b002bd3752e945e87665ff867d
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-09-28 02:35:17 -0400

    stars fade like beautifulshiny diamonds

--- browser/js/common/directives/star/star.js
+++ browser/js/common/directives/star/star.js
@@ -4,6 +4,7 @@ app.directive("star", function($animate, $rootScope) {
 		templateUrl: '',
 		link: function (scope, element, attributes) {
 			var hasDarkenedOnce;
+			var played = [];
 			scope.$on("playingNote", function (event, coords) {
 
 				if (coords === attributes.cx + "-" + attributes.cy) {
@@ -12,33 +13,29 @@ app.directive("star", function($animate, $rootScope) {
 						element.addClass("dark");
 						hasDarkenedOnce = true;
 					}
+					if (played.indexOf(coords) < 0){
+						element.addClass("fadeoutstar");
+						// played.push(coords);
+					}
 					// $rootScope.$broadcast('starFadeIn');
 					// element.addClass("fadeinstar");
 					// console.log("Next element: ", element.next());
 					element.addClass("animate");
 					// element.addClass("fadeoutstar");
 
-					// if (int1) {
-					// 	clearTimeout(int1);
+					// if (element.next().length === 0 || element.next() === 1 || !element.next()) {
+					// 	setTimeout(function(){
+					// 		// element.removeClass("fadeinstar");
+					// 		console.log("no next element");
+					// 		element.addClass("fadeoutstar");
+					// 	},500);
 					// }
-					// if (int2) {
-					// 	clearTimeout(int2);
+					// else {
+					// 	console.log(element.next());
 					// }
-
-					if (element.next().length === 0 || element.next() === 1 || !element.next()) {
-						setTimeout(function(){
-							// element.removeClass("fadeinstar");
-							console.log("no next element");
-							element.addClass("fadeoutstar");
-						},500);
-					}
-					else {
-						console.log(element.next());
-					}
 					setTimeout(function () {
 						element.removeClass("animate");
 						element.removeClass("fadeoutstar");
-
 					}, 2000);
 
 				}
@@ -54,10 +51,12 @@ app.directive("star", function($animate, $rootScope) {
 				}, 800+x);
 			});
 			scope.$on("matchingNote", function (event, coords) {
+				played.push(coords);
 					console.log("IN THE LISTENER and coords are ", coords);
 				if (coords === attributes.cx + "-" + attributes.cy) {
-					// element.removeClass('dark');
+					element.removeClass('dark');
 					element.addClass("animate");
+					element.removeClass("fadeoutstar");
 
 					setTimeout(function () {
 						element.removeClass("animate");
